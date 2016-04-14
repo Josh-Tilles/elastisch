b1113e35a82a685be954c9ebdc9cf45c7c2eba7c
Josh Tilles <josh@signafire.com>
2016-04-14 01:34:31 -0400

    tmp! Fix failing count-with-mixed-mappings tests.
    Q.v. <https://www.elastic.co/guide/en/elasticsearch/reference/2.0/breaking_20_mapping_changes.html#_conflicting_field_mappings>.

--- test/clojurewerkz/elastisch/native_api/count_test.clj
+++ test/clojurewerkz/elastisch/native_api/count_test.clj
@@ -44,8 +44,10 @@
 
   (deftest ^{:native true} test-count-with-mixed-mappings
     (let [index-name "people"
-          index-type "person"]
-      (idx/create conn index-name :mappings fx/people-mapping)
+          index-type "person"
+          person-properties (:person fx/people-mapping)]
+      (idx/create conn index-name :mappings {:person person-properties
+                                             :altperson person-properties})
       (doc/create conn index-name index-type fx/person-jack)
       (doc/create conn index-name index-type fx/person-joe)
       (doc/create conn index-name "altperson" fx/person-jack)
